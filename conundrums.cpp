a944283e1a238a22b422d3706e51023ca5cd1f91
Khalis Totorkulov <totorkulov.khalis@gmail.com>
2022-10-18 19:39:27 +0300

    Polish up interactions in the main app

--- conundrums.cpp
+++ conundrums.cpp
@@ -10,9 +10,6 @@
 #include "fast_num.h"
 #include "optimizer.h"
 
-static auto now = std::chrono::steady_clock::now;
-using std::chrono::duration;
-
 void words_conundrum(Solver &solver, const std::string &conundrum) {
     std::vector<string> result = solver(conundrum);
 
@@ -40,48 +37,29 @@ void numbers_conundrum(std::array<int, 7> input, bool verbose = false){
 
     std::array<int, 6> numbers;
     auto goal = input.back();
-    copy(begin(input), prev(end(input)), begin(numbers));
+    std::copy(begin(input), prev(end(input)), begin(numbers));
 
-    sort(begin(numbers), end(numbers), std::greater{});
+    if(std::ranges::count(numbers, goal) > 0)
+        print("There is at least one trivial solution");
 
-    for(auto prn = printer("=====\n", " ", std::format(" = {}\n", goal)); auto n : numbers) prn(n);
-
-    if(auto res = std::find(begin(numbers), end(numbers), goal); res != end(numbers))
-        print("There is at least one trivial solution, {}", *res);
-
-
-    auto start = now();
     auto solutions = search(numbers, goal);
-    print("In {} found {}\n", duration<double>(now() - start), count(solutions.size(), " candidate", "", "s"));
     validate(solutions);
-    print("After validation left with {}\n", count(solutions.size(), " candidate", "", "s"));
     optimize(solutions);
-    print("After optimization left with {}\n", count(solutions.size(), " candidate", "", "s"));
     winnow_solutions(solutions);
-    print("After winnowing left with {}\n", count(solutions.size(), " candidate", "", "s"));
-    // std::cin.get();
-    std::ranges::sort(solutions, {}, &Solution::ind);
-    if (verbose){
-        for(auto &sol: solutions) {
-            print("{}, 0b{:b}, ", sol.ind, sol.expression);
-            for(auto prn = printer("{",", ","}, "); auto i: sol.numbers) prn(i);
-            for(auto prn = printer("{",", ","}, "); auto op: sol.operations) prn(op);
-            print("\"{}\"\n", sol.infix);
-        }
-
-    }
-    else for(auto &sol: solutions) print("{: >4}) {}={}\n", sol.ind, sol.infix, goal);
+    print("Found {}\n", count(solutions.size(), " solution", ", here it is:", "s, here are 10:"));
+    auto take_10 = std::ranges::views::take(10);
+    for(auto &sol: solutions | take_10)
+        print("{} = {}\n", sol.infix, goal);
 }
 
 int main() {
-//     using std::chrono::duration;
     using namespace std;
     Solver solver;
 
     while (true) {
         string line;
 
-        print("Enter puzzle: ");
+        print("Enter puzzle: ", 0);
         getline(std::cin, line);
         if (line.empty()) break;
         if (regex_match(line, regex{"[^[:digit:]]+"}))
